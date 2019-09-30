f797d9fc302ed16e530779ff40af6f0ce70cbff6
rubiden <rubidendiazgranados@gmail.com>
2019-09-29 21:23:05 -0500

    prueba de angular para sophos solutions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoProductosComponent } from '../app/components/productos/listado-productos/listado-productos.component';
+import { ListadoPedidosComponent } from './components/pedidos/listado-pedidos/listado-pedidos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListadoProductosComponent, pathMatch: 'full' },
+  { path: 'pedidos', component: ListadoPedidosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
+import { ListadoProductosComponent } from './components/productos/listado-productos/listado-productos.component';
+import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {FileUploadModule} from 'primeng/fileupload';
+import { ListadoPedidosComponent } from './components/pedidos/listado-pedidos/listado-pedidos.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    ListadoProductosComponent,
+    ListadoPedidosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    CalendarModule,
+    BrowserAnimationsModule,
+    NgSelectModule,
+    FileUploadModule,
+    DataTablesModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
